1fdb6e9b39db679c77754a0df74adc5984406cff
Alec Armbruster <alecsamuelarmbruster@gmail.com>
2018-08-17 12:54:33 -0700

    Add files via upload

---
+++ client/serverconfig.php
@@ -0,0 +1,18 @@
+<?php
+
+$mainDomain = "heatsink.io";
+
+$serverIP = "openvpn-server-ip-here";
+$sshPort = "22";
+$serverUser = "root";
+$serverPass = "password";
+$serverName = "hide.dog, location: france";
+$genCMD = "echo hi";
+$genPurgeCMD = "echo hi";
+$genBreak = "echo '# [heatsink is finishing up]'";
+
+$clentPHPErrorMessage = "fail: php error?";
+$clientUnableConnectMessage = "fail: unable to establish connection";
+$clientSuccessMessage = "success: heatsink is finishing up";
+
+?>
